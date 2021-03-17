66f297fcadc0bd1eeb821ce003e727fc47c8471a
keisuke.ueno <eesema@icloud.com>
2021-03-17 12:21:20 +0000

    マルチ認証実装中＂
    git commit -m マルチ認証実装中＂

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-
+uuu
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin' ], function(){
+
+ Route::group(['prefix' => 'admin' ], function(){
 //Route::group(['prefix' => 'admin','middleware' => 'auth:admin' ], function(){
     
     
